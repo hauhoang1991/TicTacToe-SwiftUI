781b80ce6cb142a04b799fb39ca25babc9644416
Hau Hoang <hauhoangduc@gmail.com>
2022-10-28 13:40:56 -0700

    Initial Commit

---
+++ TicTacToe-SwiftUI/TicTacToe_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToe_SwiftUIApp.swift
+//  TicTacToe-SwiftUI
+//
+//  Created by Hau Hoang on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToe_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
